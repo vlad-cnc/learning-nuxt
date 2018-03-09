f1faf7056c538fa28eeb50720d2451885edcb2e1
Vladimir Curcic <vladimircurcic@canicinteractive.com>
2018-03-09 13:30:22 +0100

    Adding links and navigating around

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,6 @@
 <template>
   <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        learning-nuxt
-      </h1>
-      <h2 class="subtitle">
-        Learn that vue bro
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
-    </div>
+    <nuxt-link to="/users">Users</nuxt-link>
   </section>
 </template>
 
